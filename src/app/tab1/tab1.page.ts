1de0be051d967d078e467d817d5fd44e668e7277
rangke <c14180243@john.petra.ac.id>
2021-04-27 21:23:51 +0800

    pertama

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { LoadingController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,54 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(
+    private http : HttpClient,
+    private loadCtrl : LoadingController,
+    private toastCtrl : ToastController
+  ) {}
+
+  dataPOST = [];
+  post : any = {};
+  loading : any;
+
+  public async loaderPresent(): Promise<any> {
+
+    const loading = await this.loadCtrl.create({
+      message: "LOADING ...",
+      backdropDismiss: true
+    });
+    await loading.present();
+
+    return loading;
+  }
+
+  ionViewDidEnter(){
+    this.getDataPost();
+  }
+
+  submit(){
+    this.http.post("http://jsonplaceholder.typicode.com/posts", this.post).subscribe((res:any) => {
+      console.log(res);
+      this.toastCtrl.create({
+        duration : 3000,
+        message : "ID for new Item is " + res.id
+      }).then(l => l.present())
+      
+    });
+  }
+
+  async getDataPost(){
+
+    this.loading = await this.loaderPresent();
+
+    this.http.get("http://jsonplaceholder.typicode.com/posts").subscribe((res : any) => {
+      console.log(res);
+
+      this.dataPOST = res;
+      if (this.loading) {
+        this.loading.dismiss();
+      }
+    });
+  }
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { LoadingController, ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +10,49 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(
+    private http : HttpClient,
+    private loadCtrl : LoadingController,
+    private toastCtrl : ToastController,
+    public router : Router
+  ) {}
+
+  dataPOST = [];
+  post : any = {};
+  loading : any;
+
+
+  ionViewDidEnter(){
+    this.getDataPost();
+  }
+
+  public async loaderPresent(): Promise<any> {
+
+    const loading = await this.loadCtrl.create({
+      message: "LOADING ...",
+      backdropDismiss: true
+    });
+    await loading.present();
+
+    return loading;
+  }
+
+  async getDataPost(){
+
+    this.loading = await this.loaderPresent();
+
+    this.http.get("https://reqres.in/api/users?page=2").subscribe((res : any) => {
+      console.log(res.data);
+
+      this.dataPOST = res.data;
+      if (this.loading) {
+        this.loading.dismiss();
+      }
+    });
+  }
+
+  next(){
+    this.router.navigate(['tab3'])
+  }
 
 }
